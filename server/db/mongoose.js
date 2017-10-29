7c3709ce2865cf6b1ecc083d41ee1a4c27445f86
Kool Havis <havisk@gmail.com>
2017-10-29 00:03:11 -0400

    edits

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,8 @@ let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+    useMongoClient: true,
+});
 
 module.exports = { mongoose };
