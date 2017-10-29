7189fc22760c6199d1dc61cbeebf3f05b44e543e
Kool Havis <havisk@gmail.com>
2017-10-28 23:33:35 -0400

    edits

---
+++ server/db/mongoose.js
@@ -0,0 +1,10 @@
+let mongoose = require('mongoose');
+
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+    useMongoClient: true,
+});
+
+module.exports = { mongoose };
