cc6c250aa539b4dadebd0bb597cd0a51a9bf2380
Kool Havis <havisk@gmail.com>
2017-10-29 00:12:17 -0400

    edits

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
     useMongoClient: true,
 });
 
