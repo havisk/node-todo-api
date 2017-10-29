204eac87891bb179b2f479673dbba3933c6f2f14
Kool Havis <havisk@gmail.com>
2017-10-28 23:07:01 -0400

    edits

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.MONGODB_URI || db.mlab, {
+mongoose.connect( Process.env.MONGO_URI || db.mlab, {
     useMongoClient: true,
 });
 
