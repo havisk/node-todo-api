ae301abca2c610e142d740f82e1db4a25555d780
Kool Havis <havisk@gmail.com>
2017-10-30 13:39:26 -0400

    test the delet /todos by id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -101,3 +101,42 @@ describe('GET /todos/:id', () => {
             .end(done)
     });
 });
+
+describe('DELETE/todos/id', () => {
+    it('should delete a todo', (done) => {
+        let hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) =>{
+                if(err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toBeNull();
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+        let id = new ObjectID();
+
+        request(app)
+            .delete(`/todos/${id.toHexString()}`)
+            .expect(404)
+            .end(done)
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+            .delete('/todos/123rgh')
+            .expect(404)
+            .end(done)
+    });
+});
